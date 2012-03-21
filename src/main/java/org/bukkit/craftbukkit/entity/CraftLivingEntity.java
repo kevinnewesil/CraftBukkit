842e18c78ddbdc1ab8eebbe978a32538bc7f4fdf
TomyLobo <tomylobo@nurfuerspam.de>
2012-03-21 16:31:30 +0100

    Implemented LivingEntity.hasLineOfSight(Entity).

--- src/main/java/org/bukkit/craftbukkit/entity/CraftLivingEntity.java
+++ src/main/java/org/bukkit/craftbukkit/entity/CraftLivingEntity.java
@@ -24,6 +24,7 @@ import org.bukkit.craftbukkit.CraftServer;
 import org.bukkit.craftbukkit.CraftWorld;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.Egg;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.HumanEntity;
 import org.bukkit.entity.EnderPearl;
@@ -283,5 +284,10 @@ public class CraftLivingEntity extends CraftEntity implements LivingEntity {
 
     public EntityType getType() {
         return EntityType.UNKNOWN;
+    }
+
+    @Override
+    public boolean hasLineOfSight(Entity other) {
+        return getHandle().h(((CraftEntity) other).getHandle());
     }
 }
